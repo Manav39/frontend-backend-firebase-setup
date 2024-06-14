77aab5e84cbc6245ea2d2777735224280f69c34f
Priyam Aggarwal <122437027+priyamaggarwal18@users.noreply.github.com>
2024-06-14 22:56:30 +0530

    adding Home route with figma design

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,7 @@ import GoogleLogin from "./pages/GoogleLogin";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
+import Home from "./pages/Home";
 function App() {
   return (
     <Routes>
@@ -17,6 +18,7 @@ function App() {
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
       <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/home" element={<Home/>}/>
     </Routes>
   );
 }
